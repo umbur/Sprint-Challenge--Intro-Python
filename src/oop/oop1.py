916291e88ee38baf247bf3290ef52f365b671162
umbur <you@example.com>
2019-11-01 11:58:18 -0600

    oop1 done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,4 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
-# Initial commit 
+# Initial commit
+
+# Base Class Vehicle
+class Vehicle(object):
+    pass
+
+# Child Class GroundVehicle which inherits from Vehicle class
+class GroundVehicle(Vehicle):
+    pass
+
+# Child class Car which inherits from GroundVehicle class
+class Car(GroundVehicle):
+    pass
+
+# class Motorcycle which inherits from GroundVehicle class
+class Motorcycle(GroundVehicle):
+    pass
+
+# Child class FlightVehicle which inherits from Vehicle class
+class FlightVehicle(Vehicle):
+    pass
+
+# Child class Airplane which inherits from FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+# Child class Starship which inherits from FlightVehicle
+class Starship(FlightVehicle):
+    pass
